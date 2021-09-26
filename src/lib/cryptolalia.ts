27b819315a395392919a55f83dfe8127fb9beb3d
Gaubee <GaubeeBangeel@Gmail.com>
2021-09-26 22:13:05 +0800

    :sparkles: 导出ChatsApp模块的实例

--- src/lib/cryptolalia.ts
+++ src/lib/cryptolalia.ts
@@ -1,4 +1,4 @@
-import { ModuleStroge, Resolve } from "@bfchain/util-dep-inject";
+import { Inject, ModuleStroge, Resolve } from "@bfchain/util-dep-inject";
 import {
   Cryptolalia,
   CryptolaliaConfig,
@@ -7,7 +7,10 @@ import {
 } from "@bfchain/cryptolalia-tree";
 import { TimeHelper } from "@bfchain/cryptolalia-tree/lib/TimeHelper.web";
 import { FilesystemStorage } from "@bfchain/cryptolalia-tree/lib/Storage.fs.web";
-// import { TimeHelper } from "@bfchain/cryptolalia-tree/test";
+import {
+  ChatsApp,
+  ChatsAppHelper,
+} from "@bfchain/cryptolalia-tree/lib/ChatsApp";
 
 const moduleMap = new ModuleStroge();
 {
@@ -58,18 +61,24 @@ export type MyMessage = {
 }
 const syncChannel1: CryptolaliaSync.Channel<MyMessage> = {
   postMessage(msg) {},
-  onMessage(cb) {
+  get onMessage() {
+    return syncChannel2.postMessage;
+  },
+  set onMessage(cb) {
     syncChannel2.postMessage = cb;
   },
 };
 const syncChannel2: CryptolaliaSync.Channel<MyMessage> = {
   postMessage(msg) {},
-  onMessage(cb) {
+  get onMessage() {
+    return syncChannel1.postMessage;
+  },
+  set onMessage(cb) {
     syncChannel1.postMessage = cb;
   },
 };
-
 import { CryptoHelper } from "@bfchain/cryptolalia-tree/lib/CryptoHelper.web";
+import type { CryptolaliaTypes } from "../../../src/@types";
 
 const moduleMap1 = new ModuleStroge(
   [[CryptolaliaSync.ARGS.SYNC_CHANNE, syncChannel1]],
@@ -104,34 +113,10 @@ export const cryptolalia2 = Resolve<Cryptolalia<MyMessage>>(
   Cryptolalia,
   moduleMap2,
 );
+
 export const test = async () => {
-  // console.log(cryptolalia1.config);
   await cryptolalia1.storage.del([]);
   await cryptolalia2.storage.del([]);
-
-  // console.log(getInjectionToken(FilesystemStorage));
-  // console.log(Reflect.getMetadata("design:paramtypes", Storage));
-
-  // console.log(cryptolalia1.timelineTree.storage.targetDir);
-  // await cryptolalia1.timelineTree.addLeaf(Buffer.from("hi"), Date.now());
-  // console.log(await cryptolalia1.timelineTree.getBranchRoute(Date.now()));
-  // for (let i = 0; i < 10; ++i) {
-  //   cryptolalia1.dataList.addItem("hi~" + i + "~hi", startTime + i * 1e4);
-  // }
-
-  // for await (const msg of cryptolalia1.dataList.ItemReader(
-  //   +new Date(1631798288511.563),
-  //   1,
-  // )) {
-  //   console.log(`[${new Date(msg.createTime).toLocaleString()}] ${msg.data}`);
-  // }
-  // console.log("----");
-  // for await (const msg of cryptolalia1.dataList.ItemReader(
-  //   +new Date(1631798288511.563),
-  //   -1,
-  // )) {
-  //   console.log(`[${new Date(msg.createTime).toLocaleString()}] ${msg.data}`);
-  // }
 
   console.assert(
     await cryptolalia1.addMsg({
@@ -148,24 +133,126 @@ export const test = async () => {
     }),
   );
 
-  debugger;
-  // console.log(await cryptolalia1.timelineTree.getBranchRoute(Date.now()));
   await cryptolalia1.sync.doSync();
   await cryptolalia2.sync.doSync();
   console.log("sync done");
-
-  // console.group("cryptolalia1");
-  // const dataList1 = await cryptolalia1.getMsgList(Date.now());
-  // console.log(dataList1);
-  // console.groupEnd();
-
-  // console.group("cryptolalia2");
-  // const dataList2 = await cryptolalia2.getMsgList(Date.now());
-  // console.log(dataList2);
-  // console.groupEnd();
-
-  // console.assert(
-  //   JSON.stringify(dataList1) === JSON.stringify(dataList2),
-  //   "sync fail",
-  // );
 };
+
+const appModuleMap1 = new ModuleStroge([], moduleMap);
+const appModuleMap2 = new ModuleStroge([], moduleMap);
+type MySessionInfo = {
+  nickname: string;
+  badge: number;
+  lastMsgPreview: string;
+  lastMsgTime: number;
+  isCollection: boolean;
+};
+{
+  type AppChannel = CryptolaliaTypes.MessageChannel<
+    ChatsApp.ChatsMsg<MySessionInfo, CryptolaliaTypes.Msg<MyMessage>>
+  >;
+  const appChannel1: AppChannel = {
+    postMessage(msg) {},
+    get onMessage() {
+      return appChannel2.postMessage;
+    },
+    set onMessage(cb) {
+      appChannel2.postMessage = cb;
+    },
+  };
+  const appChannel2: AppChannel = {
+    postMessage(msg) {},
+    get onMessage() {
+      return appChannel1.postMessage;
+    },
+    set onMessage(cb) {
+      appChannel1.postMessage = cb;
+    },
+  };
+  const APP_CHANNEL = Symbol("appChannel");
+  appModuleMap1.set(APP_CHANNEL, appChannel1);
+  appModuleMap2.set(APP_CHANNEL, appChannel2);
+
+  const LOCAL_NAME_ID = Symbol("localId");
+  appModuleMap1.set(LOCAL_NAME_ID, "app1");
+  appModuleMap2.set(LOCAL_NAME_ID, "app2");
+
+  class MyChatsAppHelper extends ChatsAppHelper<
+    {
+      nickname: string;
+      badge: number;
+      lastMsgPreview: string;
+      lastMsgTime: number;
+      isCollection: boolean;
+    },
+    MyMessage
+  > {
+    handshakeSession?: <I extends unknown>(
+      sessionId: string,
+      swapIn?: I,
+    ) => BFChainUtil.PromiseMaybe<void | (I extends unknown ? unknown : never)>;
+    @Inject(APP_CHANNEL)
+    appChannel!: AppChannel;
+    @Inject(LOCAL_NAME_ID)
+    localId!: string;
+
+    getSessionId(sessionInfo: {
+      nickname: string;
+      badge: number;
+      lastMsgPreview: string;
+      lastMsgTime: number;
+      isCollection: boolean;
+    }): string {
+      return [this.localId, sessionInfo.nickname].sort().join("-");
+    }
+    compare(
+      a: {
+        nickname: string;
+        badge: number;
+        lastMsgPreview: string;
+        lastMsgTime: number;
+        isCollection: boolean;
+      },
+      b: {
+        nickname: string;
+        badge: number;
+        lastMsgPreview: string;
+        lastMsgTime: number;
+        isCollection: boolean;
+      },
+    ): number {
+      if (a.isCollection && !b.isCollection) {
+        return -1;
+      }
+      if (b.isCollection && !a.isCollection) {
+        return 1;
+      }
+      return b.lastMsgTime - a.lastMsgTime;
+    }
+    sendMessage(
+      sessionInfo: {
+        nickname: string;
+        badge: number;
+        lastMsgPreview: string;
+        lastMsgTime: number;
+        isCollection: boolean;
+      },
+      msg: ChatsApp.SessionMsg<
+        MyMessage,
+        CryptolaliaTypes.Msg<unknown, unknown>
+      >,
+    ): unknown {
+      throw new Error("Method not implemented.");
+    }
+    public get onNewMessage() {
+      return this.appChannel.onMessage;
+    }
+    public set onNewMessage(value) {
+      this.appChannel.onMessage = value;
+    }
+    // handshakeSession?: <I extends unknown>(sessionId: string, swapIn?: I) => BFChainUtil.PromiseMaybe<void | (I extends unknown ? unknown : never)>;
+  }
+  Resolve(MyChatsAppHelper, moduleMap);
+}
+export const chatsApp1 = Resolve(ChatsApp, appModuleMap1);
+export const chatsApp2 = Resolve(ChatsApp, appModuleMap2);
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { inspect } from "node:util";
+import typescript from "typescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +12,63 @@ export default defineConfig({
     outDir: "docs",
   },
   base: "./",
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    (() => {
+      const tsconfigFilepath = path.join(
+        fileURLToPath(import.meta.url),
+        "../tsconfig.json",
+      );
+      console.log(tsconfigFilepath);
+      const parsedTsConfig: typescript.TranspileOptions = new Function(
+        `return ${fs.readFileSync(tsconfigFilepath, "utf-8").trim()}`,
+      )();
+      parsedTsConfig.compilerOptions.emitDeclarationOnly = false;
+      parsedTsConfig.compilerOptions.noEmit = false;
+      parsedTsConfig.compilerOptions.sourcemap = false;
+      parsedTsConfig.compilerOptions.inlineSources = false;
+      parsedTsConfig.compilerOptions.inlineSourceMap = false;
+
+      function printDiagnostics(...args) {
+        console.log(inspect(args, false, 10, true));
+      }
+
+      return {
+        name: "tsc.emitDecoratorMetadata",
+        load(source) {
+          if (!parsedTsConfig?.compilerOptions?.emitDecoratorMetadata) {
+            return null;
+          }
+
+          try {
+            const ts = fs.readFileSync(source, "utf8");
+            if (!ts) {
+              return null;
+            }
+
+            // Find the decorator and if there isn't one, return out
+            const hasDecorator = ts
+              .replace(
+                /`(?:\.|(\\\`)|[^\``])*`|"(?:\.|(\\\")|[^\""\n])*"|'(?:\.|(\\\')|[^\''\n])*'/g,
+                "",
+              )
+              .replace(/\/\/[\w\W]*?\n/g, "")
+              .replace(/\/\*[\w\W]*?\*\//g, "")
+              .includes("@");
+            if (!hasDecorator) {
+              return null;
+            }
+
+            // console.log("need emitDecoratorMetadata", source);
+            const program = typescript.transpileModule(ts, parsedTsConfig);
+            // console.log(program.outputText);
+            return program.outputText;
+          } catch (err) {
+            printDiagnostics({ file: source, err });
+          }
+          return null;
+        },
+      };
+    })(),
+  ],
 });
